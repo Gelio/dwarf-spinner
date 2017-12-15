9fb07e2f4f91371dfd22900ef6877498a85644eb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-15 21:14:35 +0100

    load dwarf model prototype

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import { ModelPrototypeLoader } from 'services/ModelPrototypeLoader';
 
 window.onload = bootstrap;
 
-function bootstrap() {
-  const mainCanvas = document.getElementById('main-canvas');
+async function bootstrap() {
+  const mainCanvas = <HTMLCanvasElement>document.getElementById('main-canvas');
 
   if (!mainCanvas) {
     throw new Error('Main canvas not found');
   }
 
-  const modelPrototypeLoader = new ModelPrototypeLoader();
-  modelPrototypeLoader.loadModel('models/dwarf.json');
+  const gl = mainCanvas.getContext('webgl');
+  if (!gl) {
+    throw new Error('WebGL not supported');
+  }
+
+  const modelPrototypeLoader = new ModelPrototypeLoader(gl);
+  const modelPrototype = await modelPrototypeLoader.loadModelPrototype('models/dwarf.json');
+
+  console.log(modelPrototype);
 }
