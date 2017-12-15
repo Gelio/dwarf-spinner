46f9b5f886872614d86210cf1cf86aea27db915e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-15 11:01:08 +0100

    add binding in buffer facade

--- src/models/WebGLBufferFacade.ts
+++ src/models/WebGLBufferFacade.ts
@@ -5,6 +5,8 @@ export class WebGLBufferFacade {
   public readonly itemSize: number;
   public readonly itemsCount: number;
 
+  private readonly gl: WebGLRenderingContext;
+
   public constructor(
     gl: WebGLRenderingContext,
     data: ArrayBufferView,
@@ -12,11 +14,24 @@ export class WebGLBufferFacade {
     itemsCount: number,
     usageType: BufferUsageType
   ) {
+    this.gl = gl;
+
     this.itemSize = itemSize;
     this.itemsCount = itemsCount;
 
     this.buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, data, usageType);
+  }
+
+  public bindArrayBufferToAttribute(attribute: number) {
+    const gl = this.gl;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.vertexAttribPointer(attribute, this.itemSize, gl.FLOAT, false, 0, 0);
+  }
+
+  public bindElementArrayBuffer() {
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.buffer);
   }
 }
