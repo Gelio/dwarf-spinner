d00e833289d33b0c4b4a096a6d643672da1b6c9f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-15 10:50:28 +0100

    add texture facade

---
+++ src/models/TextureFacade.ts
@@ -0,0 +1,37 @@
+export class Texture {
+  public readonly texture: WebGLTexture | null;
+  public readonly id: number;
+
+  private readonly gl: WebGLRenderingContext;
+  private readonly textureNumber: number;
+
+  /**
+   * @param gl
+   * @param image
+   * @param id Texture number (starting from 0)
+   */
+  public constructor(gl: WebGLRenderingContext, image: HTMLImageElement, id: number) {
+    this.gl = gl;
+    this.id = id;
+    this.textureNumber = gl.TEXTURE0 + this.id;
+
+    this.texture = gl.createTexture();
+
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  }
+
+  public activate(uniformLocation: WebGLUniformLocation | null) {
+    const gl = this.gl;
+
+    gl.activeTexture(this.textureNumber);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.uniform1i(uniformLocation, this.id);
+  }
+}
