db2dd4f3f20d394c173f21673a526c6a6c86b07f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-15 23:13:10 +0100

    add main Application class
    Currently responsible for everything, work in progress.
    
    Displays a dwarf on a plane

---
+++ src/Application.ts
@@ -0,0 +1,196 @@
+import { mat4, vec3 } from 'gl-matrix';
+
+import { BufferUsageType } from 'common/BufferUsageType';
+import { ShaderType } from 'common/ShaderType';
+
+import { Camera } from 'models/Camera';
+import { WebGLArrayBufferFacade } from 'models/WebGLArrayBufferFacade';
+import { WebGLElementArrayBufferFacade } from 'models/WebGLElementArrayBufferFacade';
+import { WebGLProgramFacade } from 'models/WebGLProgramFacade';
+
+import { ImageLoader } from 'services/ImageLoader';
+import { ModelPrototypeLoader } from 'services/ModelPrototypeLoader';
+import { ShaderCompiler } from 'services/ShaderCompiler';
+
+// tslint:disable no-require-imports import-name no-var-requires
+const fragmentShaderSource = require('./shaders/fragment-shader.glsl');
+const vertexShaderSource = require('./shaders/vertex-shader.glsl');
+// tslint:enable no-require-imports, import-name
+
+export class Application {
+  private readonly canvas: HTMLCanvasElement;
+  private readonly gl: WebGLRenderingContext;
+
+  private readonly camera: Camera;
+  private programFacade: WebGLProgramFacade;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+
+    const gl = this.canvas.getContext('webgl');
+    if (!gl) {
+      throw new Error('WebGL not supported');
+    }
+
+    this.gl = gl;
+
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    // tslint:disable-next-line:no-bitwise
+    this.gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    this.camera = new Camera(
+      vec3.fromValues(1, 10, -15),
+      vec3.fromValues(0, 5, 0)
+    );
+  }
+
+  // tslint:disable-next-line:max-func-body-length
+  public async run() {
+    const gl = this.gl;
+    this.initProgram();
+
+    const imageLoader = new ImageLoader();
+    const modelPrototypeLoader = new ModelPrototypeLoader(this.gl, imageLoader);
+    const modelPrototype = await modelPrototypeLoader.loadModelPrototype(
+      'models/dwarf.json',
+      'models/texture.bmp'
+    );
+
+    const program = this.programFacade.program;
+    const vertexPositionAttribute = gl.getAttribLocation(
+      program,
+      'aVertexPosition'
+    );
+    gl.enableVertexAttribArray(vertexPositionAttribute);
+
+    const textureCoordsAttribute = gl.getAttribLocation(
+      program,
+      'aTextureCoords'
+    );
+    gl.enableVertexAttribArray(textureCoordsAttribute);
+
+    const modelMatrixUniform = <WebGLUniformLocation>gl.getUniformLocation(
+      program,
+      'uModelMatrix'
+    );
+    const viewMatrixUniform = <WebGLUniformLocation>gl.getUniformLocation(
+      program,
+      'uViewMatrix'
+    );
+    const projectionMatrixUniform = <WebGLUniformLocation>gl.getUniformLocation(
+      program,
+      'uProjectionMatrix'
+    );
+    const textureSamplerUniform = <WebGLUniformLocation>gl.getUniformLocation(
+      program,
+      'uTextureSampler'
+    );
+
+    const projectionMatrix = mat4.create();
+    mat4.perspective(
+      projectionMatrix,
+      45,
+      this.canvas.width / this.canvas.height,
+      0.1,
+      100
+    );
+
+    const modelMatrix = mat4.create();
+    mat4.identity(modelMatrix);
+
+    gl.uniformMatrix4fv(modelMatrixUniform, false, modelMatrix);
+    gl.uniformMatrix4fv(viewMatrixUniform, false, this.camera.viewMatrix);
+    gl.uniformMatrix4fv(projectionMatrixUniform, false, projectionMatrix);
+
+    modelPrototype.texture.activate(textureSamplerUniform);
+
+    // prettier-ignore
+    const planeVertices = [
+      -5, 0, -5,
+      -5, 0, 5,
+      5, 0, 5,
+      5, 0, -5
+    ];
+
+    const planeVerticesBuffer = new WebGLArrayBufferFacade(
+      gl,
+      new Float32Array(planeVertices),
+      3,
+      4,
+      BufferUsageType.StaticDraw
+    );
+
+    // prettier-ignore
+    const planeIndices = [
+      0, 1, 2,
+      0, 2, 3
+    ];
+
+    const planeIndicesBuffer = new WebGLElementArrayBufferFacade(
+      gl,
+      new Uint16Array(planeIndices),
+      1,
+      6,
+      BufferUsageType.StaticDraw
+    );
+
+    // prettier-ignore
+    const planeTextureCoords = [
+      -5, -5,
+      -5, 5,
+      5, 5,
+      5, -5
+    ];
+
+    const planeTextureCoordsBuffer = new WebGLArrayBufferFacade(
+      gl,
+      new Float32Array(planeTextureCoords),
+      2,
+      4,
+      BufferUsageType.StaticDraw
+    );
+
+    planeTextureCoordsBuffer.bind(textureCoordsAttribute);
+    planeVerticesBuffer.bind(vertexPositionAttribute);
+    planeIndicesBuffer.bind();
+
+    gl.drawElements(gl.TRIANGLES, planeIndicesBuffer.itemsCount, gl.UNSIGNED_SHORT, 0);
+
+    mat4.scale(modelMatrix, modelMatrix, [50, 50, 50]);
+    mat4.rotateY(modelMatrix, modelMatrix, Math.PI);
+    gl.uniformMatrix4fv(modelMatrixUniform, false, modelMatrix);
+
+    modelPrototype.vertexPositionBuffer.bind(vertexPositionAttribute);
+    modelPrototype.vertexTextureCoordsBuffer.bind(textureCoordsAttribute);
+
+    modelPrototype.vertexIndexBuffer.bind();
+    gl.drawElements(
+      gl.TRIANGLES,
+      modelPrototype.vertexIndexBuffer.itemsCount,
+      gl.UNSIGNED_SHORT,
+      0
+    );
+  }
+
+  private initProgram() {
+    const shaderCompiler = new ShaderCompiler(this.gl);
+
+    const vertexShader = shaderCompiler.compileShader(
+      vertexShaderSource,
+      ShaderType.VertexShader
+    );
+    const fragmentShader = shaderCompiler.compileShader(
+      fragmentShaderSource,
+      ShaderType.FragmentShader
+    );
+
+    this.programFacade = new WebGLProgramFacade(
+      this.gl,
+      vertexShader,
+      fragmentShader
+    );
+    this.programFacade.use();
+  }
+}
