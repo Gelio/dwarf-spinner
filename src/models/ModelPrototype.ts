860bcf2cb32c207f966b50550bd757c9225f430f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-15 11:01:14 +0100

    add model prototype

---
+++ src/models/ModelPrototype.ts
@@ -0,0 +1,31 @@
+import { WebGLBufferFacade } from 'models/WebGLBufferFacade';
+import { WebGLTextureFacade } from 'models/WebGLTextureFacade';
+
+export class ModelPrototype {
+  public vertexNormalBuffer: WebGLBufferFacade;
+  public vertexTextureCoordsBuffer: WebGLBufferFacade;
+  public vertexPositionBuffer: WebGLBufferFacade;
+  public vertexIndexBuffer: WebGLBufferFacade;
+
+  public texture: WebGLTextureFacade;
+
+  private readonly gl: WebGLRenderingContext;
+
+  public constructor(
+    gl: WebGLRenderingContext,
+    vertexNormalBuffer: WebGLBufferFacade,
+    vertexTextureCoordsBuffer: WebGLBufferFacade,
+    vertexPositionBuffer: WebGLBufferFacade,
+    vertexIndexBuffer: WebGLBufferFacade,
+    texture: WebGLTextureFacade
+  ) {
+    this.gl = gl;
+
+    this.vertexNormalBuffer = vertexNormalBuffer;
+    this.vertexTextureCoordsBuffer = vertexTextureCoordsBuffer;
+    this.vertexPositionBuffer = vertexPositionBuffer;
+    this.vertexIndexBuffer = vertexIndexBuffer;
+
+    this.texture = texture;
+  }
+}
