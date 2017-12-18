d244f99f48231f10a338ad14ed9d18c6afc7f6ce
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-18 15:34:26 +0100

    update Model
    Allow model to draw itself

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,20 +1,54 @@
 import { Body } from 'cannon';
-import { mat4 } from 'gl-matrix';
+import { mat4, quat } from 'gl-matrix';
 
 import { ModelPrototype } from 'models/ModelPrototype';
+
+import { MatrixTransformer } from 'services/MatrixTransformer';
+import { WebGLBinder } from 'WebGLBinder';
 
 export class Model {
   public readonly modelPrototype: ModelPrototype;
   public readonly body: Body;
   // TODO: lights
-  // TODO: body
 
   public readonly modelMatrix: mat4;
 
-  public constructor(modelPrototype: ModelPrototype, body: Body) {
+  private readonly matrixTransformer: MatrixTransformer;
+
+  public constructor(matrixTransformer: MatrixTransformer, modelPrototype: ModelPrototype, body: Body) {
+    this.matrixTransformer = matrixTransformer;
     this.modelPrototype = modelPrototype;
     this.body = body;
 
     this.modelMatrix = mat4.clone(this.modelPrototype.modelMatrix);
+  }
+
+  public draw(gl: WebGLRenderingContext, webGLBinder: WebGLBinder) {
+    this.modelPrototype.bindBuffersAndTexture(webGLBinder);
+
+    this.updateModelMatrixFromBody();
+    webGLBinder.bindModelMatrix(this.modelMatrix);
+
+    gl.drawElements(
+      gl.TRIANGLES,
+      this.modelPrototype.vertexIndexBuffer.itemsCount,
+      gl.UNSIGNED_SHORT,
+      0
+    );
+  }
+
+  private updateModelMatrixFromBody() {
+    const { body } = this;
+
+    const quaternion = quat.fromValues(body.quaternion.x, body.quaternion.y, body.quaternion.z, body.quaternion.w);
+    const quaternionMatrix = mat4.fromQuat(this.modelMatrix, quaternion);
+
+    mat4.mul(this.modelMatrix, quaternionMatrix, this.modelPrototype.modelMatrix);
+
+    this.matrixTransformer.translate(this.modelMatrix, [
+      body.position.x,
+      body.position.z,
+      body.position.y
+    ]);
   }
 }
