f1d9ab1544fe9757ed42292ff7275715b35df6a7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-21 13:33:36 +0100

    use Model interface in Renderer

--- src/services/Renderer.ts
+++ src/services/Renderer.ts
@@ -1,7 +1,7 @@
 import { mat4 } from 'gl-matrix';
 
+import { Model } from 'interfaces/Model';
 import { Camera } from 'models/Camera';
-import { Model } from 'models/Model';
 
 import { WebGLBinder } from 'services/WebGLBinder';
 
