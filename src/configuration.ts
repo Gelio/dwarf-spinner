419672e4f8f39199152b5bd3b261eb4d42bcc47d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-21 22:13:08 +0100

    add ambient light to configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,4 +1,7 @@
+import { vec4 } from 'gl-matrix';
+
 export const configuration = {
   maxPhysicsWorldTimeAdvance: 1 / 30,
-  physicsSpeed: 1
+  physicsSpeed: 1,
+  ambientLightColor: vec4.fromValues(0.2, 0.2, 0.2, 1)
 };
