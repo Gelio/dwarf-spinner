cf3b5955cf2bbd649a8eb199a31edafd32e4bab6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-21 16:01:06 +0100

    add fidget spinner loading, add constraints (WIP)
    Need to force fidget spinner to stay vertically

--- src/services/WorldLoader.ts
+++ src/services/WorldLoader.ts
@@ -1,4 +1,15 @@
-import { Body, Box, Plane, Vec3, World } from 'cannon';
+import {
+  Body,
+  Box,
+  Cylinder,
+  DistanceConstraint,
+  // LockConstraint,
+  // HingeConstraint,
+  Plane,
+  PointToPointConstraint,
+  Vec3,
+  World
+} from 'cannon';
 import { mat4 } from 'gl-matrix';
 
 import { ApplicationWorld } from 'models/ApplicationWorld';
@@ -24,9 +35,23 @@ export class WorldLoader {
 
     await Promise.all([
       this.loadGround(world),
-      this.loadCubeModel(world),
-      this.loadDwarf(world)
+      this.loadDwarf(world),
+      this.loadFidgetSpinner(world)
     ]);
+
+    const dwarfLocalPoint = new Vec3(0, 0, 0.75);
+    const fidgetSpinnerLocalPoint = new Vec3(1.2, 0, 0);
+    const constraint = new PointToPointConstraint(
+      world.dwarf.body,
+      dwarfLocalPoint,
+      world.fidgetSpinner.body,
+      fidgetSpinnerLocalPoint
+    );
+    world.physicsWorld.addConstraint(constraint);
+
+    setTimeout(() => {
+      world.physicsWorld.removeConstraint(constraint);
+    }, 3000);
 
     return world;
   }
@@ -69,44 +94,68 @@ export class WorldLoader {
     }
   }
 
-  private async loadCubeModel(applicationWorld: ApplicationWorld) {
-    const cubePrototype = await this.modelPrototypeLoader.loadModelPrototype(
-      'assets/models/cube.json',
-      'assets/textures/f16-texture.bmp'
-    );
-
-    const cubeBody = new Body({ mass: 5, position: new Vec3(0, 0, 5) });
-    const shape = new Box(new Vec3(0.5, 0.5, 0.5));
-    cubeBody.addShape(shape);
-    applicationWorld.physicsWorld.addBody(cubeBody);
-
-    const cube = new PhysicalModel(cubePrototype, cubeBody);
-    applicationWorld.models.push(cube);
-  }
-
   private async loadDwarf(applicationWorld: ApplicationWorld) {
     const dwarfPrototype = await this.modelPrototypeLoader.loadModelPrototype(
       'assets/models/Dwarf_2_Very_Low-centered.json',
       'assets/textures/dwarf_2_1K_color.jpg'
     );
 
-    const rotationAxis = CoordinateConverter.physicsToRendering(
-      new Vec3(0, 0, 1)
-    );
-    mat4.rotate(
-      dwarfPrototype.modelMatrix,
-      dwarfPrototype.modelMatrix,
-      Math.PI,
-      rotationAxis
-    );
-
-    const dwarfBody = new Body({ mass: 70, position: new Vec3(2, 2, 5) });
+    const dwarfBody = new Body({ mass: 70, position: new Vec3(2, 2, 3.05) });
     const shape = new Box(new Vec3(0.4, 0.3, 0.75));
     dwarfBody.addShape(shape);
     applicationWorld.physicsWorld.addBody(dwarfBody);
 
+    dwarfBody.quaternion.setFromAxisAngle(new Vec3(0, 1, 0), Math.PI / 2);
+
     const dwarf = new PhysicalModel(dwarfPrototype, dwarfBody);
     applicationWorld.models.push(dwarf);
     applicationWorld.dwarf = dwarf;
+  }
+
+  private async loadFidgetSpinner(applicationWorld: ApplicationWorld) {
+    const fidgetSpinnerPrototype = await this.modelPrototypeLoader.loadModelPrototype(
+      'assets/models/Fidget_Spinner-centered.json',
+      'assets/textures/fidget_texture.png'
+    );
+
+    const radius = 1.2;
+    const height = 0.05;
+
+    const constantBody = new Body({
+      mass: 0,
+      position: new Vec3(2, 2, 5.01)
+    });
+    applicationWorld.physicsWorld.addBody(constantBody);
+
+    const fidgetSpinnerBody = new Body({
+      mass: 400,
+      position: new Vec3(2, 2, 5)
+    });
+    const shape = new Cylinder(radius, radius, height, 10);
+    fidgetSpinnerBody.addShape(shape);
+    applicationWorld.physicsWorld.addBody(fidgetSpinnerBody);
+
+    fidgetSpinnerBody.angularDamping = 0;
+    fidgetSpinnerBody.angularVelocity.z = 5;
+
+    fidgetSpinnerBody.quaternion.setFromAxisAngle(new Vec3(1, 0, 0), Math.PI / 2);
+
+    setInterval(() => {
+      fidgetSpinnerBody.angularVelocity.z += 5;
+    }, 500);
+
+    const constraint = new DistanceConstraint(
+      fidgetSpinnerBody,
+      constantBody,
+      1
+    );
+    applicationWorld.physicsWorld.addConstraint(constraint);
+
+    const fidgetSpinner = new PhysicalModel(
+      fidgetSpinnerPrototype,
+      fidgetSpinnerBody
+    );
+    applicationWorld.models.push(fidgetSpinner);
+    applicationWorld.fidgetSpinner = fidgetSpinner;
   }
 }
