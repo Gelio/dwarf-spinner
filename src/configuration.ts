6fdd6111c735fa7c5d8a9cbd8b7094eda83ea9e2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-22 10:26:48 +0100

    add default values for illumination coefficients and model types

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,8 @@
 import { Vec3 } from 'cannon';
 import { vec3 } from 'gl-matrix';
 
+import { IlluminationModelType } from 'common/IlluminationModelType';
+
 import { CoordinateConverter } from 'services/CoordinateConverter';
 
 export const configuration = {
@@ -9,5 +11,9 @@ export const configuration = {
   ambientLightColor: vec3.fromValues(0.2, 0.2, 0.2),
 
   pointLightColor: vec3.fromValues(1, 1, 1),
-  pointLightPosition: CoordinateConverter.physicsToRendering(new Vec3(0, 0, 5))
+  pointLightPosition: CoordinateConverter.physicsToRendering(new Vec3(0, 0, 5)),
+
+  diffuseCoefficient: 1,
+  specularCoefficient: 0.5,
+  illuminationModelType: IlluminationModelType.Phong
 };
