643ed2f70d75001942cba02fe866341525a70b0d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-22 21:19:23 +0100

    use illumination properties in models

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,8 +2,14 @@ import { Vec3 } from 'cannon';
 import { vec3 } from 'gl-matrix';
 
 import { IlluminationModelType } from 'common/IlluminationModelType';
+import { IlluminationProperties } from 'common/IlluminationProperties';
 
 import { CoordinateConverter } from 'services/CoordinateConverter';
+
+const defaultIlluminationProperties = new IlluminationProperties();
+defaultIlluminationProperties.diffuseCoefficient = 0.8;
+defaultIlluminationProperties.specularCoefficient = 0.5;
+defaultIlluminationProperties.specularShininess = 50;
 
 export const configuration = {
   maxPhysicsWorldTimeAdvance: 1 / 30,
@@ -13,9 +19,6 @@ export const configuration = {
   pointLightColor: vec3.fromValues(1, 1, 1),
   pointLightPosition: CoordinateConverter.physicsToRendering(new Vec3(0, 0, 5)),
 
-  defaultSpecularShininess: 50,
-
-  diffuseCoefficient: 0.8,
-  specularCoefficient: 0.8,
-  illuminationModelType: IlluminationModelType.Phong
+  illuminationModelType: IlluminationModelType.Phong,
+  defaultIlluminationProperties
 };
