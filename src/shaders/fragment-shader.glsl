ddaf2d30da388119e4b9663a7c0da74923fe0e52
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-22 10:48:13 +0100

    use Phong's illumination model in fragment shader

--- src/shaders/fragment-shader.glsl
+++ src/shaders/fragment-shader.glsl
@@ -7,20 +7,47 @@ uniform vec3 uAmbientLightColor;
 uniform vec3 uPointLightPosition;
 uniform vec3 uPointLightColor;
 
+uniform vec3 uViewerPosition;
+
+uniform float uDiffuseCoefficient;
+uniform float uSpecularCoefficient;
+uniform int uIlluminationModelType;
+uniform float uSpecularShininess;
+
 varying vec3 vPosition;
 varying vec3 vNormalVector;
 varying vec2 vTextureCoords;
 
-vec3 getColorFromPointLight() {
-  vec3 lightVector = normalize(uPointLightPosition - vPosition);
 
-  return uPointLightColor * max(0.0, dot(vNormalVector, lightVector));
-}
+vec3 getDiffuseLightIntensity(vec3 lightVector);
+vec3 getSpecularLightIntensity(vec3 lightVector);
 
 
 void main(void) {
-  vec4 ambientLight = vec4(uAmbientLightColor, 1.0);
-  vec4 pointLightColor = vec4(getColorFromPointLight(), 1.0);
+  vec3 lightVector = normalize(uPointLightPosition - vPosition);
 
-  gl_FragColor = (ambientLight + pointLightColor) * texture2D(uTextureSampler, vTextureCoords);
+  vec3 diffuseIntensity = getDiffuseLightIntensity(lightVector);
+  vec3 specularIntensity = getSpecularLightIntensity(lightVector);
+
+  vec4 lightIntensity = vec4(uAmbientLightColor + diffuseIntensity + specularIntensity, 1.0);
+
+  gl_FragColor = lightIntensity * texture2D(uTextureSampler, vTextureCoords);
+}
+
+
+vec3 getDiffuseLightIntensity(vec3 lightVector) {
+  // Should sum across all light sources
+  float cosine = max(0.0, dot(vNormalVector, lightVector));
+
+  return uDiffuseCoefficient * uPointLightColor * cosine;
+}
+
+vec3 getSpecularLightIntensity(vec3 lightVector) {
+  // Should sum across all light sources
+  vec3 viewerVector = normalize(uViewerPosition - vPosition);
+  vec3 reflectionVector = 2.0 * dot(vNormalVector, lightVector) * vNormalVector - lightVector;
+
+  float cosine = max(0.0, dot(viewerVector, reflectionVector));
+
+  return uSpecularCoefficient * uPointLightColor * pow(cosine, uSpecularShininess);
 }
