8e66a8dc6686fe3c6e0896ae6846c374880bc13e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-24 14:33:57 +0100

    add proper input handling

--- src/services/input/InputHandler.ts
+++ src/services/input/InputHandler.ts
@@ -1,4 +1,7 @@
+import { Vec3 } from 'cannon';
 import { Set as ImmutableSet } from 'immutable';
+
+import { configuration } from 'configuration';
 
 import { KeyboardKeys } from 'common/KeyboardKeys';
 
@@ -16,6 +19,8 @@ export class InputHandler {
   private readonly eventEmitter: ApplicationEventEmitter;
 
   private dwarfReleased = false;
+  private hingeAngle = 0;
+  private readonly hingeRotationAxis = new Vec3(0, 0, 1);
 
   constructor(world: ApplicationWorld, eventEmitter: ApplicationEventEmitter) {
     this.world = world;
@@ -36,26 +41,15 @@ export class InputHandler {
   }
 
   public step(_timeDelta: number) {
-    const dwarfBody = this.world.dwarf.body;
     const pressedKeys: ImmutableSet<number> = store.getState().input.getIn(['pressedKeys']);
 
-    // FIXME: add proper rotation
-    if (pressedKeys.has(KeyboardKeys.ArrowUp)) {
-      dwarfBody.angularVelocity.x += 0.5;
-    }
-    if (pressedKeys.has(KeyboardKeys.ArrowDown)) {
-      dwarfBody.angularVelocity.x -= 0.5;
-    }
-    if (pressedKeys.has(KeyboardKeys.ArrowLeft)) {
-      dwarfBody.angularVelocity.y -= 0.5;
-    }
-    if (pressedKeys.has(KeyboardKeys.ArrowRight)) {
-      dwarfBody.angularVelocity.y -= 0.5;
-    }
+    this.rotateFidgetSpinner(pressedKeys, _timeDelta);
+    this.rotateHinge(pressedKeys, _timeDelta);
   }
 
   private restartWorld() {
     this.world.models.forEach(model => model.reset());
+    this.hingeAngle = 0;
 
     if (this.dwarfReleased) {
       this.world.physicsWorld.addConstraint(this.world.dwarfConstraint);
@@ -70,5 +64,39 @@ export class InputHandler {
 
     this.world.physicsWorld.removeConstraint(this.world.dwarfConstraint);
     this.dwarfReleased = true;
+  }
+
+  private rotateFidgetSpinner(pressedKeys: ImmutableSet<number>, timeDelta: number) {
+    const fidgetSpinnerBody = this.world.fidgetSpinner.body;
+    const tickAcceleration = configuration.spinnerAngularAcceleration * timeDelta;
+
+    if (pressedKeys.has(KeyboardKeys.ArrowUp)) {
+      fidgetSpinnerBody.angularVelocity.x += tickAcceleration;
+    }
+    if (pressedKeys.has(KeyboardKeys.ArrowDown)) {
+      fidgetSpinnerBody.angularVelocity.x -= tickAcceleration;
+    }
+  }
+
+  private rotateHinge(pressedKeys: ImmutableSet<number>, timeDelta: number) {
+    const hingeBody = this.world.fidgetSpinnerHinge.body;
+    const tickAcceleration = timeDelta * configuration.hingeAngularAcceleration;
+
+    const rotateLeft = pressedKeys.has(KeyboardKeys.ArrowLeft) ? 1 : -1;
+    const rotateRight = pressedKeys.has(KeyboardKeys.ArrowRight) ? 1 : -1;
+
+    if (rotateLeft * rotateRight === 1) {
+      // Either both on or both off
+      return;
+    }
+
+    if (rotateLeft > 0) {
+      this.hingeAngle += tickAcceleration;
+    }
+    if (rotateRight > 0) {
+      this.hingeAngle -= tickAcceleration;
+    }
+
+    hingeBody.quaternion.setFromAxisAngle(this.hingeRotationAxis, this.hingeAngle);
   }
 }
