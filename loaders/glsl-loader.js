6fb1cd319929fda3267ce6119321f2ac13dd442b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-26 10:13:54 +0100

    add recursive glsl loader

---
+++ loaders/glsl-loader.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const fs = require('fs');
+
+const requireRegex = /#pragma require\(('|")(.*)('|")\)/gi;
+const moduleExportsRegex = /module\.exports = "(.*)"/gi;
+
+/**
+ * @param {string} source
+ */
+module.exports = function loader(source) {
+  requireRegex.lastIndex = 0;
+  const callback = this.async();
+  let dependencies = 0;
+  let completedDependencies = 0;
+  let completed = false;
+
+  while (true) {
+    const matches = requireRegex.exec(source);
+
+    if (!matches) {
+      break;
+    }
+
+    const dependencyPath = matches[2];
+    this.addDependency(dependencyPath);
+    dependencies++;
+
+    this.loadModule(dependencyPath, (err, dependencySource) => {
+      if (completed) {
+        return;
+      }
+
+      if (err) {
+        completed = true;
+        return callback(err);
+      }
+
+      dependencySource = moduleExportsRegex.exec(dependencySource)[1];
+      dependencySource = dependencySource.replace(/\\n/g, '\n').replace(/\\r/g, '\r');
+      source = source.replace(matches[0], dependencySource);
+      completedDependencies++;
+
+      if (completedDependencies === dependencies) {
+        completed = true;
+        callback(null, source);
+      }
+    });
+  }
+
+  if (dependencies === 0) {
+    callback(null, source);
+  }
+}
