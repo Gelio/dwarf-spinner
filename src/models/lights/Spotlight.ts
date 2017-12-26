f89b886e9d60c367599c4c6d1899aef835d7d021
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-12-26 11:31:06 +0100

    refactor spotlight, add binding

--- src/models/lights/Spotlight.ts
+++ src/models/lights/Spotlight.ts
@@ -1,15 +1,23 @@
 import { vec3 } from 'gl-matrix';
 
 import { DirectedLight } from 'interfaces/lights/DirectedLight';
+import { WebGLBinder } from 'services/WebGLBinder';
 
 export class Spotlight implements DirectedLight {
-  public direction: vec3;
-  public position: vec3;
+  public direction: vec3 = vec3.create();
+  public position: vec3 = vec3.create();
   public color: vec3;
+  public cutoff: number;
 
-  public constructor(color: vec3, position: vec3, direction: vec3) {
+  public constructor(color: vec3, cutoffAngle: number) {
     this.color = color;
-    this.position = position;
-    this.direction = direction;
+    this.cutoff = Math.cos(cutoffAngle);
+  }
+
+  public bind(webGLBinder: WebGLBinder) {
+    webGLBinder.bindSpotlightColor(this.color);
+    webGLBinder.bindSpotlightDirection(this.direction);
+    webGLBinder.bindSpotlightPosition(this.position);
+    webGLBinder.bindSpotlightCutoff(this.cutoff);
   }
 }
